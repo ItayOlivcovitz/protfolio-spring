570ec505ffa98d990606fabfc8a9f356b62014fc
ItayOlivcovitz <104628872+ItayOlivcovitz@users.noreply.github.com>
2025-01-12 19:04:36 +0200

    Update saveVisitorInfo.js

--- src/main/resources/static/assets/js/saveVisitorInfo.js
+++ src/main/resources/static/assets/js/saveVisitorInfo.js
@@ -13,7 +13,7 @@ export async function saveVisitorInfo() {
         console.log('Visitor Info to Save:', visitorInfo); // Log the gathered visitor info
 
         // Send a POST request to the Visitor Tracker API
-        const response = await fetch('https://itay-olivcovitz-portfolio.up.railway.app/api/save', {
+        const response = await fetch('https://itay-olivcovitz-portfolio.up.railway.app/api/visitor/save', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
